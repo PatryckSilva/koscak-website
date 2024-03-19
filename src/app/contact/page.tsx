fbf37d88a2aac2b77438dfbef99d4aa190eac10f
Patryck Silva <patrycksilva70@gmail.com>
2024-03-18 22:21:41 -0300

    fix: diminuindo padding da page de contato

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -3,7 +3,7 @@ import { NextPage } from "next";
 
 const About: NextPage = () => {
   return (
-    <main className="relative flex min-h-dvh w-full flex-col items-center justify-center gap-9 bg-white py-20 bg-grid-small-black/[0.2] dark:bg-slate-950 dark:bg-grid-small-white/[0.2]  md:py-24  ">
+    <main className="relative flex min-h-dvh w-full flex-col items-center justify-center gap-9 bg-white py-14 bg-grid-small-black/[0.2] dark:bg-slate-950 dark:bg-grid-small-white/[0.2]  md:py-24">
       <FormFrame />
     </main>
   );
